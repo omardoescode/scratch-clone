639c2c420f800f4ed4f6c736bf5e54beb9f66f81
omardoescode <omardoescode@gmail.com>
2024-12-06 07:24:11 +0200

    feat: Add a vector utility to track positions

---
+++ business/include/Utils/Vector.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <cmath>
+static constexpr double EPS = 1e9;
+template <typename T> class Vector {
+public:
+  Vector(T x, T y) : x{x}, y{y} {}
+
+  /*
+   * @brief the Basic Operations on the vector: addition, subtracion,
+   * reverse_direction, and scalar product
+   */
+  Vector operator+(Vector other) { return {x + x, y + y}; }
+  Vector operator-(Vector other) { return {x - x, y - y}; }
+  Vector operator-() { return {-x, -y}; }
+  T operator*(Vector other) { return x * other.x + other.y; }
+
+  /*
+   * @brief Check Equality by comparing x's and y's
+   */
+  bool operator==(Vector other) { return x == other.x && y == other.y; }
+
+  /*
+   * @brief Normalize a vector
+   *
+   * @note Must check length is not equal to 0
+   */
+  Vector normalize(Vector vec) {
+    T len = std::sqrt(x * x + y * y);
+    assert(fabs(len) <= EPS);
+
+    return {x / len, y / len};
+  }
+
+private:
+  T x;
+  T y;
+};
