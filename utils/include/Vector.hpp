bc646287ec1108f31ee642fd61906ba322e14055
omardoescode <omardoescode@gmail.com>
2025-01-08 16:51:27 +0200

    feat: Fix Bugs in Vector class

--- utils/include/Vector.hpp
+++ utils/include/Vector.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <cmath>
-static constexpr double EPS = 1e9;
+static constexpr double EPS = 1e-9;
+
 template <typename T> class Vector {
 public:
   Vector(T x, T y) : x{x}, y{y} {}
@@ -9,9 +10,9 @@ public:
    * @brief the Basic Operations on the vector: addition, subtracion,
    * reverse_direction, and scalar product
    */
-  Vector operator+(Vector other) { return {x + x, y + y}; }
-  Vector operator-(Vector other) { return {x - x, y - y}; }
+  Vector operator+(Vector other) { return {x + other.x, y + other.y}; }
   Vector operator-() { return {-x, -y}; }
+  Vector operator-(Vector other) { return *this + -other; }
   T operator*(Vector other) { return x * other.x + other.y; }
 
   /*
@@ -26,7 +27,8 @@ public:
    */
   Vector normalize() {
     T len = length();
-    assert(fabs(len) <= EPS);
+    assert(len);
+    assert(fabs(len) >= EPS);
 
     return {x / len, y / len};
   }
